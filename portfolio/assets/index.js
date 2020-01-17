341cc6bb70b4a40a786a03fb833ce2378ce0de43
Lukas Mark Jakobsen <109243@RTS-365.dk>
2020-01-17 23:15:15 +0100

    test

---
+++ portfolio/assets/index.js
@@ -0,0 +1,11 @@
+let buttonTarget = document.querySelector(".button_target");
+let sectionTwo = document.querySelector(".section__two");
+let wrapper = document.querySelector("#wrapper")
+console.log(sectionTwo);
+
+buttonTarget.addEventListener('click', function(){
+    window.scrollTo(0, 1000)
+    // sectionTwo.scrollTop = sectionTwo.scrollHeight;
+    
+
+})
